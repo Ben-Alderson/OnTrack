bc020803ae0ebef906e7663924acb5dd613ec63e
Ben-Alderson <bennyalderson@gmail.com>
2018-03-24 11:13:42 -0400

    Make tabs work in plugin

---
+++ settings_page/tabs.js
@@ -0,0 +1,17 @@
+function openOption() {
+		var optName = this.dataset.tabid;
+    var tabIndex, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (tabIndex = 0; tabIndex < tabcontent.length; tabIndex++) {
+        tabcontent[tabIndex].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (tabIndex = 0; tabIndex < tablinks.length; tabIndex++) {
+        tablinks[tabIndex].className = tablinks[tabIndex].className.replace(" active", "");
+    }
+    document.getElementById(optName).style.display = "block";
+    event.currentTarget.className += " active";
+}
+
+document.querySelectorAll(".tablinks").forEach((i) => { i.addEventListener("click", openOption) });
+document.getElementById("defaultOpen").click();
